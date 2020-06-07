4a3a25df59dbbcd6f17795a949828a4446ece22a
Roger <chenqi830521@gmail.com>
2020-06-07 13:52:50 +1000

    Sever with listening to port 5555

---
+++ 5555.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 5555;
+
+const server = http.createServer((req, res) =>{
+	res.statusCode = 200;
+	res.setHearder('Content-Type', 'text/plain');
+	res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () =>{
+	console.log('Server running at http://${hostname}:${port}/');
+});
+
+//app.get('/', (req, res) => res.send('Hello World!'));
+
+//app.listen(port, () => console.log('Example app listening on port ${port}!'));
